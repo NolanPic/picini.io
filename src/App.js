d5b3ce91de216e605acb4dc2812d08097f0e01ff
Nolan Picini <nolanpic@gmail.com>
2020-07-07 16:26:02 -0700

    Separate home page content + add react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import avatar from "./images/me.jpeg";
 import "./sass/index.scss";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -30,105 +32,11 @@ function App() {
         </div>
       </section>
 
-      <section id="about">
-        <div>
-          <h2>About</h2>
-          <p>
-            I am a full-stack web developer building modern web applications. I
-            appreciate good design, both visually and in code. Always curious.
-          </p>
-        </div>
-        <div>
-          <h2>Skilled with</h2>
-          <ul>
-            <li>JS/React/Vue/jQuery</li>
-            <li>ASP.NET/C#</li>
-            <li>SQL (SQL Server, Postgres)</li>
-            <li>Debugging in Chrome DevTools</li>
-          </ul>
-        </div>
-      </section>
-
-      <section id="history">
-        <h2>History</h2>
-        <table>
-          <tr>
-            <th>Yrs</th>
-            <th>Company</th>
-            <th>Title</th>
-            <th>Skills</th>
-          </tr>
-          <tr>
-            <td>6.5</td>
-            <td>EfinityTech</td>
-            <td>Web developer</td>
-            <td>ASP.NET</td>
-          </tr>
-        </table>
-      </section>
-
-      <section id="portfolio">
-        <h2>Portfolio</h2>
-        <table>
-          <tr>
-            <th>Project</th>
-            <th>Description</th>
-            <th>Work done/skills used</th>
-          </tr>
-
-          <tr>
-            <td>Proprietary CMS</td>
-            <td>
-              An in-house CMS built for 100s of clients to maintain their
-              websites
-            </td>
-            <td>
-              Contributed to development and maintenance of a proprietary CMS
-              with ASP.NET, SQL, JS, Bootstrap
-            </td>
-          </tr>
-
-          <tr>
-            <td>Business directory for local city</td>
-            <td>
-              Collaborated with a local city chamber of commerce to design and
-              develop a business directory tourism website
-            </td>
-            <td>
-              Utilized Vue, Google Maps API, and REST API. Where SEO was
-              important, used ASP.NET server-driven pages
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <a
-                href="https://john-conways-game-of-life.now.sh/"
-                target="_blank"
-              >
-                Conway's Game of Life
-              </a>
-            </td>
-            <td>
-              Implemented Conway’s Game of Life using modern web technologies
-            </td>
-            <td>
-              Wrote game logic in vanilla JS, UI in React, and grid utilizing
-              CSS Grid
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <a href="https://neumorphic-todo.now.sh/" target="_blank">
-                Neu todo
-              </a>
-            </td>
-            <td>How I todo</td>
-            <td>React/styled components</td>
-          </tr>
-        </table>
-      </section>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
 
       <footer>
         <p>Copyright 2020. Fueled by coffee, books, and exploring the pnw. 🌧</p>
