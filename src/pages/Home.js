6af1db8ac885086b3625797f67cf095d26649d8f
Nolan Picini <nolanpic@gmail.com>
2020-07-13 15:15:15 -0700

    replace tables with cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,88 +24,110 @@ const Home = () => {
       </section>
 
       <section id="history">
-        <h2>History</h2>
-        <div className="table-wrap">
-          <table>
-            <tr>
-              <th>Yrs</th>
-              <th>Company</th>
-              <th>Title</th>
-              <th>Skills</th>
-            </tr>
-            <tr>
-              <td>6.5</td>
-              <td>EfinityTech</td>
-              <td>Web developer</td>
-              <td>ASP.NET</td>
-            </tr>
-          </table>
+        <h2>Experience</h2>
+
+        <div className="card-wrap">
+          <div className="card">
+            <h3 className="card-title">EfinityTech</h3>
+            <div className="card-body">
+              <ul>
+                <li>Years: 6.5</li>
+                <li>Title: Web developer</li>
+                <li>Skills: ASP.NET/JS</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
 
       <section id="portfolio">
         <h2>Projects</h2>
-        <div className="table-wrap">
-          <table>
-            <tr>
-              <th>Project</th>
-              <th>Description</th>
-              <th>Work done/skills used</th>
-            </tr>
+        <div className="card-wrap">
+          {/* CMS card */}
+          <div className="card">
+            <h3 className="card-title">
+              <Link to="/cms">Proprietary CMS</Link>
+            </h3>
 
-            <tr>
-              <td>
-                <Link to="/cms">Proprietary CMS</Link>
-              </td>
-              <td>
+            <div className="description">
+              <h4>Description</h4>
+              <p>
                 An in-house CMS built for 100s of clients to maintain their
-                websites
-              </td>
-              <td>
+                websites.
+              </p>
+            </div>
+
+            <div className="work-done">
+              <h4>Work done/skills used</h4>
+              <p>
                 Contributed to development and maintenance of a proprietary CMS
                 with ASP.NET, SQL, JS, Bootstrap
-              </td>
-            </tr>
+              </p>
+            </div>
+          </div>
 
-            <tr>
-              <td>Business directory for local city</td>
-              <td>
+          {/* Business directory card */}
+          <div className="card">
+            <h3 className="card-title">Business directory for local city</h3>
+            <div className="description">
+              <h4>Description</h4>
+              <p>
                 Collaborated with a local city chamber of commerce to design and
-                develop a business directory tourism website
-              </td>
-              <td>
+                develop a business directory tourism website.
+              </p>
+            </div>
+
+            <div className="work-done">
+              <h4>Work done/skills used</h4>
+              <p>
                 Utilized Vue, Google Maps API, and REST API. Where SEO was
                 important, used ASP.NET server-driven pages
-              </td>
-            </tr>
+              </p>
+            </div>
+          </div>
 
-            <tr>
-              <td>
-                <Link to="/game-of-life">Conway's Game of Life</Link>
-              </td>
-              <td>
-                Implemented Conway’s Game of Life using modern web technologies
-              </td>
-              <td>
+          {/* Game of Life card */}
+          <div className="card">
+            <h3 className="card-title">
+              <Link to="/game-of-life">Conway's Game of Life</Link>
+            </h3>
+            <div className="description">
+              <h4>Description</h4>
+              <p>
+                Implemented Conway’s Game of Life using modern web technologies.
+              </p>
+            </div>
+
+            <div className="work-done">
+              <h4>Work done/skills used</h4>
+              <p>
                 Wrote game logic in vanilla JS, UI in React, and grid utilizing
                 CSS Grid
-              </td>
-            </tr>
+              </p>
+            </div>
+          </div>
 
-            <tr>
-              <td>
-                <a
-                  href="https://neumorphic-todo.now.sh/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Neu todo
-                </a>
-              </td>
-              <td>Simple todo in the style of Neumorphism</td>
-              <td>React/styled components</td>
-            </tr>
-          </table>
+          {/* Neumorphic todo card */}
+          <div className="card">
+            <h3 className="card-title">
+              <a
+                href="https://neumorphic-todo.now.sh/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Neu todo
+              </a>
+            </h3>
+            <div className="description">
+              <h4>Description</h4>
+              <p>Simple todo in the style of Neumorphism.</p>
+            </div>
+
+            <div className="work-done">
+              <h4>Work done/skills used</h4>
+              <p>React/styled components</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
